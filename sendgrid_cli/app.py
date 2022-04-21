cb4ca6bddb467f855cdeb934a8edfedaa8ba5b60
Alberto Re <alberto.re@gmail.com>
2022-04-21 13:50:28 +0200

    Add --attach (-a) option to attach a local file to the message

--- sendgrid_cli/app.py
+++ sendgrid_cli/app.py
@@ -1,9 +1,23 @@
+import base64
 import os
+from mimetypes import guess_type
+from pathlib import Path
+from uuid import uuid4
 
 import click
 from dotenv import load_dotenv
 from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+from sendgrid.helpers.mail import (
+    Attachment,
+    ContentId,
+    Disposition,
+    FileContent,
+    FileName,
+    FileType,
+    Mail,
+)
+
+ATTACHMENT_DEFAULT_CONTENT_DISPOSITION = "attachment"
 
 load_dotenv()
 
@@ -18,13 +32,31 @@ load_dotenv()
 @click.option("-t", "--to-address", help="Specify recipient(s)", required=True)
 @click.option("-s", "--subject", help="Specify subject", required=True)
 @click.option("-b", "--body", help="Specify message body", required=True)
-def sendmail(from_address, to_address, subject, body):
+@click.option("-a", "--attach", help="Specify an attachment")
+def sendmail(from_address, to_address, subject, body, attach):
     message = Mail(
         from_email=from_address,
         to_emails=to_address,
         subject=subject,
         html_content=body,
     )
+
+    if attach:
+        attachment_path = Path(attach)
+        if not attachment_path.is_file():
+            raise IOError()
+
+        with open(attachment_path, "rb") as f:
+            data = f.read()
+
+        encoded = base64.b64encode(data).decode()
+        attachment = Attachment()
+        attachment.file_content = FileContent(encoded)
+        attachment.file_type = FileType(guess_type(attachment_path)[0])
+        attachment.file_name = FileName(attachment_path.name)
+        attachment.disposition = Disposition(ATTACHMENT_DEFAULT_CONTENT_DISPOSITION)
+        attachment.content_id = ContentId(str(uuid4()))
+        message.attachment = attachment
 
     sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
     response = sg.send(message)
